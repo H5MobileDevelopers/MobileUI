a1d25a1e8790185a72d24e167e30c43cc53c9fb6
haseeAmarathunga <haseeamarathunga@gmail.com>
2020-07-30 12:24:01 +0530

    Update login UI

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,14 +8,45 @@ import {MatIconRegistry} from '@angular/material/icon';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  type = 'password';
+  darkMode = false;
+  mode = 'Light';
+  eye = 'eye';
 
   constructor(private sanitizer: DomSanitizer, private iconRegistry: MatIconRegistry) {
     iconRegistry.addSvgIcon(
         'forward',
         sanitizer.bypassSecurityTrustResourceUrl('assets/icon/arrow_forward_ios-24px.svg'));
+    iconRegistry.addSvgIcon(
+        'eye',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/icon/visibility-24px.svg'));
+    iconRegistry.addSvgIcon(
+        'clear',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/icon/clear_all-24px.svg'));
+    iconRegistry.addSvgIcon(
+        'eye-close',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/icon/visibility_off-24px.svg'));
   }
 
   ngOnInit() {
   }
 
+  viewPassword() {
+    if (this.type === 'password') {
+      this.type = 'input';
+      this.eye = 'eye-close';
+    } else {
+      this.type = 'password';
+      this.eye = 'eye';
+    }
+  }
+
+  changeTheme() {
+    this.darkMode = !this.darkMode;
+    if (this.mode === 'Light') {
+      this.mode = 'Dark';
+    } else {
+      this.mode = 'Light';
+    }
+  }
 }
